b5188a34547a4cd48df9d22d349d7a4b536decf4
pankit-eng <thapar.pankit@gmail.com>
2025-10-21 16:28:51 -0400

    centralize versioning schema

---
+++ _version.py
@@ -0,0 +1,8 @@
+"""
+Central version file for OpenEnv project.
+
+All packages in this monorepo derive their version from this file.
+To release a new version, simply update __version__ here.
+"""
+
+__version__ = "0.1.0"
