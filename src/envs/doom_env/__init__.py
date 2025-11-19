791b46fb2897f60d4f4b58f1877849dda57eaa87
Ali Sol <alisol@meta.com>
2025-11-19 11:09:48 -0800

    add the init for the doom

---
+++ src/envs/doom_env/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Doom Env Environment - A simple test environment for HTTP server."""
+
+from .client import DoomEnv
+from .models import DoomAction, DoomObservation
+
+__all__ = ["DoomAction", "DoomObservation", "DoomEnv"]
+
